11a02e8d36b8a1cc6982bd17c5c0ad6addf93ea4
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2025-05-30 03:43:16 -0500

    +

--- download_artifacts.py
+++ download_artifacts.py
@@ -1,3 +1,9 @@
+#----------------------------------------------------------------------------------------------------
+# Work done while being at the Intelligent Robotics and Vision Lab at the University of Texas, Dallas
+# Please check the licenses of the respective works utilized here before using this script.
+# 🖋️ Jishnu Jaykumar Padalunkal (2025).
+#----------------------------------------------------------------------------------------------------
+
 # Script to download Google Drive folders to specified directories
 # Downloads:
 # - https://drive.google.com/drive/folders/1tBHKf60K8DLM5arm-Chyf7jxkzOr5zGl to checkpoints/
